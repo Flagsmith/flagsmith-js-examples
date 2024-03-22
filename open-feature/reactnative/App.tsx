267db2f5c87dbe9aadd3df3ec61a0f7f6227985a
kyle-ssg <kyle@solidstategroup.com>
2024-03-22 22:14:55 +0000

    Add react native example

---
+++ open-feature/reactnative/App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {Suspense} from 'react';
+import {OpenFeature} from '@openfeature/web-sdk';
+import flagsmith from 'react-native-flagsmith';
+import {OpenFeatureProvider} from '@openfeature/react-sdk';
+import {FlagsmithProvider} from '@openfeature/flagsmith';
+import ExampleComponent from './ExampleComponent';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+OpenFeature.setProviderAndWait(
+  new FlagsmithProvider({
+    environmentID: 'QjgYur4LQTwe5HpvbvhpzK',
+    flagsmithInstance: flagsmith,
+    cacheFlags: true,
+    AsyncStorage,
+  }),
+).finally(() => {
+  alert('');
+});
+function App(): React.JSX.Element {
+  return (
+    <OpenFeatureProvider>
+      <>
+        <Suspense fallback={<></>}>
+          <ExampleComponent />
+        </Suspense>
+      </>
+    </OpenFeatureProvider>
+  );
+}
+
+export default App;
---
+++ open-feature/reactnative/ExampleComponent.tsx
@@ -0,0 +1,33 @@
+import React, {FC, useCallback} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {useNumberFlagValue} from '@openfeature/react-sdk';
+import {OpenFeature} from '@openfeature/web-sdk';
+
+type ExampleComponentType = {};
+
+const ExampleComponent: FC<ExampleComponentType> = ({}) => {
+  const font_size = useNumberFlagValue('font_size', 12); // only causes re-render if specified flag values / traits change
+  const identify = useCallback(() => {
+    OpenFeature.setContext({targetingKey: 'flagsmith_sample_user'});
+  }, []);
+  const logout = useCallback(() => {
+    OpenFeature.setContext({});
+  }, []);
+  const identity = OpenFeature.getContext()?.targetingKey;
+  return (
+    <View style={{paddingTop: 100}}>
+      <Text>font_size: {font_size}</Text>
+      {identity ? (
+        <TouchableOpacity onPress={logout}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={identify}>
+          <Text>Identify</Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default ExampleComponent;
